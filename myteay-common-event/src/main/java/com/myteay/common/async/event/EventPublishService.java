5097ba2544fa3aa3571ff4047ea6a160d30fc4be
danlley <danlley@126.com>
2017-10-29 01:05:44 +0800

    init

---
+++ myteay-common-event/src/main/java/com/myteay/common/async/event/EventPublishService.java
@@ -0,0 +1,21 @@
+/**
+ * Myteay.com Inc.
+ * Copyright (c) 2005-2017 All Rights Reserved.
+ */
+package com.myteay.common.async.event;
+
+/**
+ * 异步事件执行服务（默认支持同步）
+ * 
+ * @author min.weixm
+ * @version $Id: EventPublishService.java, v 0.1 Oct 28, 2017 11:21:57 PM min.weixm Exp $
+ */
+public interface EventPublishService<T> {
+
+    /**
+     * 发布事件
+     * 
+     * @param event     异步事件模型
+     */
+    public T publishEvent(MtEvent<?> event) throws MtException;
+}
